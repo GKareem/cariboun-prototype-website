6a8366606288636d117ba98049e22454760f97d1
GKareem <kareem.ghadban@gmail.com>
2022-03-28 19:51:21 -0400

    version 1.0

--- src/Components/NavbarData.js
+++ src/Components/NavbarData.js
@@ -4,26 +4,26 @@ export const NavbarData = [
   {
     title: "Dashboard",
     icon: <i class="bi bi-grid-1x2-fill" />,
-    link: "/dashboard",
+    link: "/cariboun-prototype-website/dashboard",
   },
   {
     title: "Reporting",
     icon: <i class="bi bi-pie-chart-fill" />,
-    link: "/reporting",
+    link: "/cariboun-prototype-website/reporting",
   },
   {
     title: "SDG",
     icon: <i class="bi bi-disc-fill" />,
-    link: "/sdg",
+    link: "/cariboun-prototype-website/sdg",
   },
   {
     title: "Tasks",
     icon: <i class="bi bi-clipboard2-check-fill" />,
-    link: "/tasks",
+    link: "/cariboun-prototype-website/tasks",
   },
   {
     title: "Environment",
     icon: <i class="bi bi-clouds-fill" />,
-    link: "/environment",
+    link: "/cariboun-prototype-website/environment",
   },
 ]
